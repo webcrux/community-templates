dc458ed97bbb60d8178fe2c983df5e0fcf179e5d
Raymond Kristiansen <raymond@nexusweb.no>
2019-07-06 12:08:42 +0200

    Updated test for 1.22

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+require 'open3'
 require 'erb'
 require 'tempfile'
 require 'nokogiri'
@@ -21,7 +22,7 @@ class PartitioningHelper
       clearpart --all --initlabel
       part / --fstype=ext4 --size=1024 --grow
       part swap  --recommended
-      DL
+    DL
     part
   end
 
@@ -54,10 +55,13 @@ class FakeNamespace
   attr_reader :root_pass, :grub_pass, :host
   def initialize(family, name, major, minor, release = nil)
     @mediapath = 'url --url http://localhost/repo/xyz'
+    @additional_media = []
     @root_pass = '$1$redhat$9yxjZID8FYVlQzHGhasqW/'
-    @grub_pass = 'blah'
-    @dynamic = false,
-    @static = false,
+    @grub_pass = '--password=blah'
+    @dynamic = false
+    @static = false
+    @preseed_server = 'example.com:80'
+    @preseed_path = '/bla'
     @host = FakeStruct.new(
       :operatingsystem => FakeStruct.new(
         :name => name,
@@ -87,27 +91,34 @@ class FakeNamespace
       ),
       :as_string => name,
       :subnet => FakeStruct.new(
-        :dhcp_boot_mode? => true
+        :dhcp_boot_mode? => true,
+        :mtu => 1500
       ),
       :mac => '00:00:00:00:00:01',
       :primary_interface => FakeStruct.new(
         :identifier => 'eth0',
         :subnet => FakeStruct.new(
-          :dhcp_boot_mode? => true
+          :dhcp_boot_mode? => true,
+          :mtu => 9000
         ),
       ),
-     :managed_interfaces => [
-       FakeStruct.new(
-         :identifier => 'eth0',
-         :managed? => true,
-         :primary => true,
-         :ip => '1.2.3.4',
-         :subnet => FakeStruct.new(
-           :dhcp_boot_mode? => true
-         ),
-       ),
-     ]
+      :managed_interfaces => [
+        FakeStruct.new(
+          :identifier => 'eth0',
+          :managed? => true,
+          :primary => true,
+          :ip => '1.2.3.4',
+          :subnet => FakeStruct.new(
+            :dhcp_boot_mode? => true,
+            :mtu => 1496
+          ),
+        ),
+      ]
     )
+  end
+
+  def plugin_present?(name)
+    false
   end
 
   def host_enc
@@ -130,12 +141,24 @@ class FakeNamespace
     ''
   end
 
+  def snippet_if_exists(*args)
+    ''
+  end
+
+  def template_name
+    'template name'
+  end
+
   def ks_console(*args)
     'console=ttyS99'
   end
 
   def foreman_url(*args)
     'http://localhost'
+  end
+
+  def indent(_, string)
+    string
   end
 end
 
@@ -148,20 +171,20 @@ module TemplatesHelper
 
   def ksvalidator(version, kickstart)
     ksvalidator_cmd = ENV['KSVALIDATOR'] || 'ksvalidator'
-    output = `#{ksvalidator_cmd} -v #{version} #{kickstart}`
-    [$?.to_i, output]
+    stdout, stderr, status = Open3.capture3("#{ksvalidator_cmd} -v #{version} #{kickstart}")
+    [status, stdout, stderr]
   end
 
   def debconfsetsel(preseed)
     debconfset_cmd = ENV['DEBCONFSETSEL'] || 'debconf-set-selections'
-    output = `#{debconfset_cmd} --checkonly #{preseed}`
-    [$?.to_i, output]
+    stdout, stderr, status = Open3.capture3("#{debconfset_cmd} --checkonly #{preseed}")
+    [status, stdout, stderr]
   end
 
   def autoyastvalidator(autoyast)
     xml = File.open(autoyast) { |f| Nokogiri::XML(f) }
     xml_errors = xml.errors
-    [xml_errors.length, xml_errors]
+    [xml_errors.length, '', xml_errors]
   end
 
   def validate_erb(template, namespace, ksversion)
@@ -184,5 +207,19 @@ end
 class String
   def blank?
     self !~ /[^[:space:]]/
+  end
+end
+
+class TemplateRenderer
+  include TemplatesHelper
+
+  def initialize(osfamily, template)
+    @osfamily = osfamily
+    @template = template
+  end
+
+  def render(distro, major, minor, distro_prefix)
+    ns = FakeNamespace.new(@osfamily, distro, major, minor)
+    validate_erb(@template, ns, "#{distro_prefix}#{major}")
   end
 end
